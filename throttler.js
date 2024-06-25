f6b4cdbbe88a5f477c1cf15fb8bbfe0a0130c570
Ben Scuron <bscuron19@gmail.com>
2024-06-25 17:35:26 -0400

    Add Throttler class

---
+++ throttler.js
@@ -0,0 +1,81 @@
+class Throttler {
+
+    #total;
+    #finished;
+    #queued;
+    #verbose;
+
+    constructor(functions=[], verbose=false) {
+        if (functions.some(fn => typeof fn !== 'function')) {
+            throw new Error('Invalid function provided to Throttler constructor');
+        }
+
+        this.#total = functions.length;
+        this.#finished = 0;
+        this.#queued = [...functions];
+        this.#verbose = verbose;
+    }
+
+    async batch(n) {
+
+        if (this.#queued.length === 0) {
+            return;
+        }
+
+        const active = this.#queued.splice(0, n);
+
+        if (this.#verbose) {
+            console.log(`Throttler: Creating batch of ${active.length} (${this.#finished}/${this.#total} complete)`);
+        }
+
+        const startTime = performance.now();
+        await Promise.all(active.map(fn => fn()));
+        this.#finished += active.length;
+
+        if (this.#verbose) {
+            const deltaTime = (performance.now() - startTime) / 1e3;
+            console.log(`Throttler: ${this.#finished}/${this.#total} (Batch Time: ${deltaTime}s)`);
+        }
+
+        await this.batch(n);
+    }
+
+    async queue(n) {
+        let promiseResolve;
+        const promise = new Promise((resolve, _) => {
+            promiseResolve = resolve;
+        });
+
+        let active = 0;
+        const queuePrime = async (fn) => {
+
+            active++;
+
+            if (this.#verbose) {
+                console.log(`Throttler: Queue Capacity ${active}/${n}`);
+            }
+
+            const startTime = performance.now();
+            const result = await fn();
+            this.#finished++;
+            active--;
+
+            if (this.#verbose) {
+                const deltaTime = (performance.now() - startTime) / 1e3;
+                console.log(`Throttler: ${this.#finished}/${this.#total} (Job Time: ${deltaTime}s)`);
+            }
+
+            if (this.#queued.length > 0 && active < n) {
+                await queuePrime(this.#queued.shift());
+            }
+
+            if (this.#total === this.#finished) {
+                promiseResolve();
+            }
+        }
+
+        this.#queued.splice(0, n).map(queuePrime);
+
+        await promise;
+    }
+}
